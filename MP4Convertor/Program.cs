a7d317497a5ed1d04ec63932360e8f58886762f3
Bacari Hersey <herseyb71@gmail.com>
2024-09-27 07:18:35 -0500

    Add project files.

---
+++ MP4Convertor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using NAudio.Lame;
+
+
+namespace MP4Convertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertMp4ToMp3("YOUR DIRECTORY", "PATH OUTPUT"); 
+        }
+
+        public static void ConvertMp4ToMp3(string inputFile, string outputFile)
+        {
+            using (var reader = new MediaFoundationReader(inputFile))
+            using (var writer = new LameMP3FileWriter(outputFile, reader.WaveFormat, LAMEPreset.VBR_90))
+            {
+                reader.CopyTo(writer);
+            }
+            Console.WriteLine("Complete!!");
+        }
+
+    }
+}
